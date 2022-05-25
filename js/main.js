3405661e8eb8e9738c6b01166a574eaa2e2144b9
Ohreum <github.com/Ohreum/test1.git>
2022-05-25 16:40:08 +0900

    message

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", () => {
-	
+
 	let project1Btn=document.getElementsByClassName("project1")[0];
 	let project2Btn=document.getElementsByClassName("project2")[0];
 	let project3Btn=document.getElementsByClassName("project3")[0];
 
 	function mobileLink(){
 		if(isMobile){
-			project1Btn.setAttribute("href","project1/mobile/index.html");			
+			project1Btn.setAttribute("href", "project1/mobile/index.html");			
 		}
 		else{
-			project1Btn.setAttribute("href","project1/pc/index.html");		
+			project1Btn.setAttribute("href", "project1/pc/index.html");		
 		}
 		project2Btn.setAttribute("href", "project2/index.html");
 		project3Btn.setAttribute("href", "project3/index.html");		
